f847af7b4fa52f6e5234af6f97625d29f9ce38bf
support-7786 <support@16londonalgo.com>
2026-08-08 22:35:13 +0500

    Bypass Next.js image optimization to fix missing images on Vercel

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,10 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
-  turbopack: {}
+  turbopack: {},
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withPWA(nextConfig);
